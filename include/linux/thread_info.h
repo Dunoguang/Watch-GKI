eaf9c6fb27caf16d620c4ed225b290c4a2fd7a52
Andy Lutomirski <luto@kernel.org>
2016-09-15 22:45:43 -0700

    sched/core: Add try_get_task_stack() and put_task_stack()
    commit c6c314a613cd7d03fb97713e0d642b493de42e69 upstream.
    
    There are a few places in the kernel that access stack memory
    belonging to a different task.  Before we can start freeing task
    stacks before the task_struct is freed, we need a way for those code
    paths to pin the stack.
    
    Signed-off-by: Andy Lutomirski <luto@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Jann Horn <jann@thejh.net>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/17a434f50ad3d77000104f21666575e10a9c1fbd.1474003868.git.luto@kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: zhangyi (F) <yi.zhang@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/thread_info.h
+++ include/linux/thread_info.h
@@ -11,9 +11,7 @@
 #include <linux/bug.h>
 #include <linux/restart_block.h>
 
-<<<<<<< HEAD
 #ifdef CONFIG_THREAD_INFO_IN_TASK
-=======
 struct timespec;
 struct compat_timespec;
 
@@ -32,7 +30,6 @@ struct thread_info {
 #define current_thread_info() ((struct thread_info *)current)
 #endif
 
->>>>>>> sched/core: Allow putting thread_info into task_struct
 /*
  * For CONFIG_THREAD_INFO_IN_TASK kernels we need <asm/current.h> for the
  * definition of current, but for !CONFIG_THREAD_INFO_IN_TASK kernels,
