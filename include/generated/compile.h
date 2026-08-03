e98ae6faa429d0a613143e92e2b054221fa971ff
Dunoguang <Dunoguang@users.noreply.github.com>
2026-08-03 16:16:13 +0800

    fs: backport EROFS to 4.4 for GSI boot
    - Backport fs/erofs from EROFS_4.9 (LZ4/LZ4HC compressed FS)
    - Upgrade lib/lz4 to v4.11 API, convert 5 old-API users
      (crypto/lz4, crypto/lz4hc, zram, squashfs, decompress_unlz4)
    - Backport CHUNKED_FILE (datalayout 4) + 64KB big pcluster support
    - Add .getxattr to erofs inode_operations for SELinux mount
    - Wire fs/Kconfig + fs/Makefile, add EROFS_SUPER_MAGIC (0xE0F5E1E2)
    - Add erofs dual-mount fstab entry to ramdisk (ext4 -> erofs fallback)
    - Enable EROFS in lineageos_dw99_defconfig
    - Remove generated kernel build config from tracking
    
    Verified: Infinity-X GSI (EROFS system) boots to launcher on S10_Max
    (SL8541e, kernel 4.4.147+ #7)

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 9 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#9 SMP PREEMPT Sun Aug 2 21:13:39 CST 2026"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "localhost"
-#define LINUX_COMPILER "gcc version 15.3.0 (Ubuntu 15.3.0-1ubuntu1) "
